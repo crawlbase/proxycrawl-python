10221bbcfafd22856cdc9986429bef461f08a83a
proxycrawl <info@proxycrawl.com>
2019-05-30 15:50:21 +0800

    Version bump 2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ readme = open('README.md').read()
 setup(
     name = 'proxycrawl',
     license = 'Apache-2.0',
-    version = '1.1.1',
+    version = '2.0.0',
     description = 'A Python class that acts as wrapper for ProxyCrawl scraping and crawling API',
     long_description = readme,
     long_description_content_type = 'text/markdown',
