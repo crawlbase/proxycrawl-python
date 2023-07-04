a28c439992b114c0ba332a2fcb1f1fa131bd514c
proxycrawl <info@proxycrawl.com>
2023-07-04 16:47:27 +0300

    docs: add deprecation notice and bump the version

--- proxycrawl/__init__.py
+++ proxycrawl/__init__.py
@@ -1,3 +1,19 @@
+# Deprecation warning
+def deprecation_warning():
+    message = """
+    ================================================================================
+    DEPRECATION WARNING - 'proxycrawl'
+    ================================================================================
+
+    'proxycrawl' is deprecated and will not be maintained. Please switch to 'crawlbase'.
+
+    More details and migration guide: https://github.com/crawlbase-source/crawlbase-python
+    ================================================================================
+    """
+    print(message)
+
+deprecation_warning()
+
 from proxycrawl.proxycrawl_api import ProxyCrawlAPI # For < 3.0 compatibility
 from proxycrawl.crawling_api import CrawlingAPI
 from proxycrawl.scraper_api import ScraperAPI
